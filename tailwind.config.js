cc3bca374d3e86cb16d812c543bb62b3a6cd98cf
Sébastien Metzger <sebastien.metzger@nomogi.org>
2022-09-08 11:22:13 +0200

    🔧 Eslint, prettier and editorconfig
    Configure eslint with prettier linked with editorconfig. And fix the default sources from the community template.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
-      },
+    content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"Inter var"', ...defaultTheme.fontFamily.sans]
+            }
+        }
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/line-clamp'),
-    require('@tailwindcss/aspect-ratio'),
-  ],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/line-clamp"),
+        require("@tailwindcss/aspect-ratio")
+    ]
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
-import VueRouter from 'unplugin-vue-router/vite'
-import { VueRouterAutoImports } from 'unplugin-vue-router'
-import Components from 'unplugin-vue-components/vite'
-import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import AutoImport from "unplugin-auto-import/vite"
+import VueRouter from "unplugin-vue-router/vite"
+import { VueRouterAutoImports } from "unplugin-vue-router"
+import Components from "unplugin-vue-components/vite"
+import { HeadlessUiResolver } from "unplugin-vue-components/resolvers"
 
 export default defineConfig({
-  plugins: [
-    VueRouter({ importMode: 'sync' }),
-    vue(),
-    Components({ resolvers: [HeadlessUiResolver()] }),
-    AutoImport({
-      imports: ['vue', '@vueuse/head', VueRouterAutoImports],
-    }),
-  ],
-  server: {
-    open: true,
-  },
+    plugins: [
+        VueRouter({ importMode: "sync" }),
+        vue(),
+        Components({ resolvers: [HeadlessUiResolver()] }),
+        AutoImport({
+            imports: ["vue", "@vueuse/head", VueRouterAutoImports],
+            eslintrc: {
+                enabled: false, // Default `false`
+                filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+                globalsPropValue: true // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+            }
+        })
+    ],
+    server: {
+        open: true
+    }
 })
